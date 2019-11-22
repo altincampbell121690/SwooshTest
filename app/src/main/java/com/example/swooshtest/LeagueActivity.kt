93d10ba41e5fa59da71e17a85d9c8cd59c4b19e8
altin <altincampbell121690@gmail.com>
2019-11-22 09:38:54 -0800

    toggle code added

--- app/src/main/java/com/example/swooshtest/LeagueActivity.kt
+++ app/src/main/java/com/example/swooshtest/LeagueActivity.kt
@@ -1,12 +1,49 @@
 package com.example.swooshtest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_league_activity.*
+import kotlinx.android.synthetic.main.activity_welcome.*
 
-class LeagueActicity : AppCompatActivity() {
+class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_league_acticity)
+        setContentView(R.layout.activity_league_activity)
+//        womensToggleBtn.setOnClickListener{
+//
+//        }
+//        mensToggleBtn.setOnClickListener{
+//
+//        }
+//        coEdToggleBtn
+
+    }
+
+    fun leagueNextClicked(view: View){
+        //val skillIntent = Intent(this,)
+    }
+
+    //change to when
+    fun onMensClicked(view:View){
+        mensToggleBtn.isChecked = true
+        womensToggleBtn.isChecked = false
+        coEdToggleBtn.isChecked= false
+
+    }
+
+    fun onWomensClicked(view:View){
+        mensToggleBtn.isChecked = false
+        womensToggleBtn.isChecked = true
+        coEdToggleBtn.isChecked= false
+
+    }
+
+    fun onCoEdClicked(view:View){
+        mensToggleBtn.isChecked = false
+        womensToggleBtn.isChecked = false
+        coEdToggleBtn.isChecked= true
     }
 }
