913a06efaed8f1873f4f089dec4a889ab2de2797
altin <altincampbell121690@gmail.com>
2019-11-24 08:59:45 -0800

    skills activity added (put extra included)

---
+++ app/src/main/java/com/example/swooshtest/LifeCycleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.swooshtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LifeCycleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
